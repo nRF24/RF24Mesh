9a4682f5885cb50024d45dfa911e61758a4b39df
Avamander <avamander@gmail.com>
2016-07-06 02:37:16 +0300

    Removed unnecessary EEPROM include. Autoformat.

--- examples/RF24Mesh_SerialConfig/RF24Mesh_SerialConfig.ino
+++ examples/RF24Mesh_SerialConfig/RF24Mesh_SerialConfig.ino
@@ -1,16 +1,15 @@
 
 /** RF24Mesh_SerialConfig.ino by TMRh20
- *
- * This example sketch shows how the same sketch can be written to a large number of devices, which are
- * configured later via Serial input.
- *
+
+   This example sketch shows how the same sketch can be written to a large number of devices, which are
+   configured later via Serial input.
+
  **/
 
 #include "RF24Network.h"
 #include "RF24.h"
 #include "RF24Mesh.h"
 #include <SPI.h>
-#include <EEPROM.h>
 #include <printf.h>
 
 /** Configure the nrf24l01 CE and CS pins */
@@ -19,14 +18,14 @@ RF24Network network(radio);
 RF24Mesh mesh(radio, network);
 
 /**
- * User Configuration: NodeID - A unique identifier for each radio. Allows addressing
- * to change dynamically with physical changes to the mesh.
- *
- * In this example, user configuration of the node takes place via Serial input.
- * Input a unique decimal value from 1-255 OR simply use characters 0-9, a-z, A-Z etc.
- * The nodeID will be set/changed and saved in EEPROM upon user input via Serial, and stored
- * between power loss, etc. Configuration only needs to be done once.
- *
+   User Configuration: NodeID - A unique identifier for each radio. Allows addressing
+   to change dynamically with physical changes to the mesh.
+
+   In this example, user configuration of the node takes place via Serial input.
+   Input a unique decimal value from 1-255 OR simply use characters 0-9, a-z, A-Z etc.
+   The nodeID will be set/changed and saved in EEPROM upon user input via Serial, and stored
+   between power loss, etc. Configuration only needs to be done once.
+
  **/
 
 
