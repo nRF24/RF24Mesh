ebbe80df848ace5ec0e57a46d1c82bf97fb0c430
Jörn Esdohr <joernesdohr@live.com>
2016-12-26 22:25:58 +0100

    wrapper functions for byte buffer parameters

--- pyRF24Mesh/pyRF24Mesh.cpp
+++ pyRF24Mesh/pyRF24Mesh.cpp
@@ -5,6 +5,51 @@
 
 namespace bp = boost::python;
 
+
+// **************** expicit wrappers *****************
+// where needed, especially where buffer is involved
+
+char *get_bytes_or_bytearray_str(bp::object buf)
+{
+    PyObject *py_ba;
+    py_ba = buf.ptr();
+    if (PyByteArray_Check(py_ba))
+        return PyByteArray_AsString(py_ba);
+    else if (PyBytes_Check(py_ba))
+        return PyBytes_AsString(py_ba);
+    else
+        throw_ba_exception();
+    return NULL;
+}
+
+int get_bytes_or_bytearray_ln(bp::object buf)
+{
+    PyObject *py_ba;
+    py_ba = buf.ptr();
+    if (PyByteArray_Check(py_ba))
+        return PyByteArray_Size(py_ba);
+    else if (PyBytes_Check(py_ba))
+        return PyBytes_Size(py_ba);
+    else
+        throw_ba_exception();
+    return 0;
+}
+
+bool write_wrap1(RF24Mesh& ref, bp::object buf, uint8_t msg_type)
+{
+	return ref.write(get_bytes_or_bytearray_str(buf), msg_type, get_bytes_or_bytearray_ln(buf));
+}
+
+bool write_wrap2(RF24Mesh& ref, bp::object buf, uint8_t msg_type, uint8_t nodeID)
+{
+	return ref.write(get_bytes_or_bytearray_str(buf), msg_type, get_bytes_or_bytearray_ln(buf), nodeID);
+}
+
+bool write_to_node_wrap(RF24Mesh& ref, uint16_t to_node,  bp::object data, uint8_t msg_type)
+{
+	return ref.write(to_node, get_bytes_or_bytearray_str(buf), msg_type, get_bytes_or_bytearray_ln(buf), nodeID);
+}
+
 // ******************** overload wrappers **************************
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(begin_overload, RF24Mesh::begin, 0, 3)
 //BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(write_overload, RF24Mesh::write, 3, 4)
@@ -15,9 +60,6 @@ BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(renewAddress_overload, RF24Mesh::renewAdd
 //
 BOOST_PYTHON_MODULE(RF24Mesh){
     { //::RF24Mesh
-        bool (RF24Mesh::*write1)(const void*, uint8_t, size_t) = &RF24Mesh::write;
-        bool (RF24Mesh::*write2)(const void*, uint8_t, size_t, uint8_t) = &RF24Mesh::write;
-
         bp::class_<RF24Mesh>("RF24Mesh", bp::init<RF24&, RF24Network&>((bp::arg("_radio"), bp::arg("_network"))))
             //bool begin(uint8_t channel = MESH_DEFAULT_CHANNEL, rf24_datarate_e data_rate = RF24_1MBPS, uint32_t timeout=MESH_RENEWAL_TIMEOUT );
             .def("begin", &RF24Mesh::begin, begin_overload(bp::args("channel", "data_rate", "timeout")))
@@ -25,8 +67,10 @@ BOOST_PYTHON_MODULE(RF24Mesh){
             .def("update", &RF24Mesh::update)
             //bool write(const void* data, uint8_t msg_type, size_t size, uint8_t nodeID=0);
             //.def("write", &RF24Mesh::write, write_overload(bp::args("data", "msg_type", "size", "nodeID")))
-            .def("write", write1)
-            .def("write", write2)
+            .def("write", &write_wrap1, (bp::arg("data"), bp::arg("msg_type")))
+            .def("write", &write_wrap2, (bp::arg("data"), bp::arg("msg_type"), bp::arg("nodeID")))
+            //bool write(uint16_t to_node, const void* data, uint8_t msg_type, size_t size );
+            .def("write", &write_to_node_wrap, (bp::arg("to_node"), bp::arg("data"), bp::arg("msg_type"), bp::arg("size")))
             //void setNodeID(uint8_t nodeID);
             .def("setNodeId", &RF24Mesh::setNodeID, (bp::arg("nodeID")))
             //void DHCP();
@@ -41,8 +85,6 @@ BOOST_PYTHON_MODULE(RF24Mesh){
             .def("releaseAddress", &RF24Mesh::releaseAddress)
             //int16_t getAddress(uint8_t nodeID);
             .def("getAddress", &RF24Mesh::getAddress, (bp::arg("nodeID")))
-            //bool write(uint16_t to_node, const void* data, uint8_t msg_type, size_t size );
-            .def("write", &RF24Mesh::write, (bp::arg("to_node"), bp::arg("data"), bp::arg("msg_type"), bp::arg("size")))
             //void setChannel(uint8_t _channel);
             .def("setChannel", &RF24Mesh::setChannel, (bp::arg("_channel")))
             //void setChild(bool allow);
