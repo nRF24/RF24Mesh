a1ecabc25bd809a8fc11635506331818190e6c5b
Brendan <2bndy5@gmail.com>
2021-06-12 04:26:42 -0700

    default BUILD_TYPE to Release

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -1,8 +1,8 @@
 # Set a default build type if none was specified
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-    message(STATUS "Setting build type to 'RelWithDebInfo' as none was specified.")
+    message(STATUS "Setting build type to 'Release' as none was specified.")
     set(CMAKE_BUILD_TYPE
-        RelWithDebInfo
+        Release
         CACHE STRING "Choose the type of build." FORCE
     )
     # Set the possible values of build type for cmake-gui, ccmake
