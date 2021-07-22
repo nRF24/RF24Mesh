fcf70ab38680651cb59988d3b699aaeec94c20fc
2bndy5 <2bndy5@gmail.com>
2021-07-21 22:12:39 -0700

    [py wrapper] augment setup.py

--- pyRF24Mesh/setup.py
+++ pyRF24Mesh/setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, Extension
 from sys import version_info
 
@@ -8,20 +9,39 @@ if version_info >= (3,):
 else:
     BOOST_LIB = "boost_python"
 
+git_dir = os.path.split(os.path.abspath(os.getcwd()))[0]
+
+# get LIB_VERSION from library.properties file for Arduino IDE
+version = "1.0"
+with open(os.path.join(git_dir, "library.properties"), "r") as f:
+    for line in f.read().splitlines():
+        if line.startswith("version"):
+            version = line.split("=")[1]
+
+
+long_description = """
+.. warning:: This python wrapper for the RF24Mesh C++ library was not intended
+    for distribution on pypi.org. If you're reading this, then this package
+    is likely unauthorized or unofficial.
+"""
+
 setup(
     name="RF24Mesh",
-    version="1.0",
-    classifiers = [
+    version=version,
+    license_files=os.path.join(git_dir, "LICENSE"),
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    classifiers=[
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "Programming Language :: C++",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    ext_modules = [
+    ext_modules=[
         Extension(
             "RF24Mesh",
-            libraries=["rf24mesh", "rf24network", BOOST_LIB],
             sources=["pyRF24Mesh.cpp"],
+            libraries=["rf24", "rf24network", "rf24mesh", BOOST_LIB],
         )
-    ]
+    ],
 )
