7ca48f0d25f1bc29a03a12ca81905d566f143135
TMRh20 <tmrh20@gmail.com>
2024-06-30 06:35:24 -0600

    mesh.checkConnection via parent not master (#250)
    * mesh.checkConnection via parent not master
    
    - Adjust mesh.checkConnection function to verify connectivity with parent only. Leave old functionality in place with a #define
    - Suggestions in place per @2bndy5
    closes #249
    
    * Fix last commit - _nodeID not nodeID
    
    * review changes
    
    * trigger compile size reports for PRs
    
    Specifically for PRs that only change the lib src code and not examples.
    
    * doc review
    
    use doxygen references
    
    * [docs] clarify return value of checkConnection()
    
    * format RF24Mesh.h
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * review supported CMake options
    
    also defaults RF24_DRIVER to SPIDEV (per nRF24/RF24#971)
    
    * add reference to new config macro in checkConnection() doc.
    
    ---------
    
    Co-authored-by: Brendan <2bndy5@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- RF24Mesh_config.h
+++ RF24Mesh_config.h
@@ -64,6 +64,25 @@
     #define MESH_CONNECTION_CHECK_ATTEMPTS 3
 #endif
 
+#define RF24MESH_CONN_CHECK_PARENT 1
+#define RF24MESH_CONN_CHECK_MASTER 0
+/**
+ * @brief How to verify a connection
+ *
+ * On child nodes, determine how they verify/check their connection periodically, or when writes start to fail via the `RF24Mesh::checkConnection();`
+ * function.
+ * Set RF24MESH_CONN_CHECK_TYPE to @ref RF24MESH_CONN_CHECK_PARENT for the new behaviour of verifying connectivity only with their parent node.
+ * Set RF24MESH_CONN_CHECK_TYPE to @ref RF24MESH_CONN_CHECK_MASTER for the old behaviour of verifying connectivity with the master node.
+ * The old behaviour typically results in more network congestion, more load on the master node, and less reliable networks,
+ * but it can work well when radio conditions are good and/or when there are only a small number of nodes on the network and/or in close proximity
+ * to the master node.
+ */
+#ifndef RF24MESH_CONN_CHECK_TYPE
+    #define RF24MESH_CONN_CHECK_TYPE RF24MESH_CONN_CHECK_PARENT
+    // To use old behavior:
+    // #define RF24MESH_CONN_CHECK_TYPE RF24MESH_CONN_CHECK_MASTER
+#endif
+
 /**************************/
 /***       Debug        ***/
 //#define RF24MESH_DEBUG_MINIMAL /** Uncomment for the Master Node to print out address assignments as they are assigned */
