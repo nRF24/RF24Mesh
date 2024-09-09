104d2a1d0a35b15673d97c23b47ee241c6e4f16e
Brendan <2bndy5@gmail.com>
2024-09-09 05:03:08 -0700

    add pyproject.toml
    ref nRF24/RF24#1000

--- pyRF24Mesh/setup.py
+++ pyRF24Mesh/setup.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
-
 import os
 from setuptools import setup, Extension
 from sys import version_info
 
-if version_info >= (3,):
-    BOOST_LIB = "boost_python3"
-else:
-    BOOST_LIB = "boost_python"
+BOOST_LIB = "boost_python" + (
+    "" if version_info < (3,) else "%d%d" % (version_info.major, version_info.minor)
+)
 
-# NOTE can't access "../../LICENSE.inc" from working dir because
+# NOTE can't access "../../library.properties" from working dir because
 # it's relative. Brute force absolute path dynamically.
 git_dir = os.path.split(os.path.abspath(os.getcwd()))[0]
 
@@ -20,26 +18,8 @@ with open(os.path.join(git_dir, "library.properties"), "r") as f:
         if line.startswith("version"):
             version = line.split("=")[1]
 
-
-long_description = """
-.. warning:: This python wrapper for the RF24Mesh C++ library was not intended
-    for distribution on pypi.org. If you're reading this, then this package
-    is likely unauthorized or unofficial.
-"""
-
 setup(
-    name="RF24Mesh",
     version=version,
-    license="GPLv2",
-    license_files=(os.path.join(git_dir, "LICENSE"),),
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    classifiers=[
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: C++",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-    ],
     ext_modules=[
         Extension(
             "RF24Mesh",
